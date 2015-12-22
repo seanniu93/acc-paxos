accca2a26406d0738d2085913c1f0e55c09d2264
Sean Niu <seanniu93@gmail.com>
2015-12-22 10:41:19 -0500

    Made members of ClientCommand private and added getters

--- src/paxos/essential/ClientCommand.java
+++ src/paxos/essential/ClientCommand.java
@@ -2,7 +2,20 @@ package paxos.essential;
 
 public class ClientCommand {
 
-	public String key;
-	public String value;
+	private String key;
+	private String value;
+
+	public ClientCommand(String key, String value) {
+		this.key = key;
+		this.value = value;
+	}
+
+	public String getKey() {
+		return key;
+	}
+
+	public String getValue() {
+		return value;
+	}
 
 }
